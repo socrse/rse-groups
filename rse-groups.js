882f9598a580ecdde1c49edbd8e8d629ba3cb077
milliams <matt@milliams.com>
2022-02-16 18:14:45 +0000

    Deploying to gh-pages from @ socrse/rse-groups@52bd11da1b8ea1225e3891a7670c07855a602b86 🚀

---
+++ rse-groups.js
@@ -0,0 +1,48 @@
+async function create_map(div) {
+    var map = L.map(div).setView([55.5, -4.], 6);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+        maxZoom: 18,
+    }).addTo(map);
+    const requestURL = 'groups.json';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const rse_groups = await response.json();
+
+    populateMap(map, rse_groups);
+}
+
+var headers = {
+    "head" : "Head of RSE",
+    "phone" : "Contact number",
+    "email" : "Contact email",
+    "postcode" : "Location",
+    "website" : "Website",
+    "twitter" : "Twitter Handle"
+}
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties) {
+        var html = [];
+        html.push(`<b>${feature.properties["name"]}</b><br>\n`)
+        html.push("<dl>")
+        for (const header in headers) {
+            if (feature.properties[header]) {
+                html.push(`<dt>${headers[header]}</dt>`)
+                html.push(`<dd>${feature.properties[header]}</dd>`)
+            }
+        }
+        html.push("</dl>")
+        layer.bindPopup(html.join("\n"));
+    }
+}
+
+function populateMap(map, data) {
+    for (const group of data) {
+        L.geoJSON(group, {
+            onEachFeature: onEachFeature
+        }).addTo(map);
+    }
+}
