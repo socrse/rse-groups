1263e15d3b0c8cf220fa35ec583ea0d732ff7eee
Matt Williams <matt@milliams.com>
2022-02-18 14:04:54 +0000

    Tweak map centre

--- rse-groups.js
+++ rse-groups.js
@@ -4,7 +4,7 @@
  */
 
 async function create_map(div) {
-    var map = L.map(div).setView([55.5, -4.], 6);
+    var map = L.map(div).setView([54, -4.], 6);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
         maxZoom: 18,
